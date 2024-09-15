a99b46a2f44b0aaa495bb63060685582357922d6
Ben Wallis <benjaminwallisuk@gmail.com>
2024-09-15 08:26:10 +0100

    added viewing page for er diagram

--- process_navigator/__init__.py
+++ process_navigator/__init__.py
@@ -2,13 +2,19 @@ from flask import Flask # Import the Flask class from the flask module, which is
 
 # import blueprints
 from process_navigator import home
+
+# import extensions
+from process_navigator.extensions import db
+from process_navigator import models
+
 # create a create_app function that initializes the Flask application, we can add other logic depending on set up
-
 def create_app():
-    app = Flask(__name__) # Create an instance of the Flask class and assign it to the variable app
+    app = Flask(__name__, instance_relative_config=True) # Create an instance of the Flask class and assign it to the variable app
     app.config['SECRET_KEY'] = 'dev' # Set the secret key of the app to 'dev'
-
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db' # Set the database URI to a SQLite database named db.sqlite
     # register blueprints
     app.register_blueprint(home.bp)
     
+    # register extensions
+    db.init_app(app)
     return app
---
+++ process_navigator/extensions.py
@@ -0,0 +1,5 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Create an instance of the SQLAlchemy class and assign it to the variable db
+# this will be our database "engine", options can be passed in here
+db = SQLAlchemy() 
