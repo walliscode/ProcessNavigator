d8f3d81175ebb776c4a451c8d2a015281116f5ad
Ben Wallis <benjaminwallisuk@gmail.com>
2025-01-03 11:42:41 +0000

    added delete methods route

--- process_navigator/extensions.py
+++ process_navigator/extensions.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import click
+from flask import current_app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
@@ -12,9 +15,14 @@ class Base(DeclarativeBase, MappedAsDataclass):
 db = SQLAlchemy(model_class=Base)
 
 
-# create a function to drop and create the database using click
+# create a function to drop and create the database using click, also emtpy file storage
 @click.command("refresh-db")
 def refresh_database_command():
     db.drop_all()
     db.create_all()
+    # delete all files in the file storage system using Pathlib
+    file_storage = Path(current_app.config["FILE_STORAGE"])
+    file_list = file_storage.glob("*")
+    for file in file_list:
+        file.unlink()
     click.echo("Database refreshed")
